cd4b9181e5170f4f09b32ca17930647a8bb748cf
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-05-20 16:31:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-function startFunc() {
+function function8() {
 	document.addEventListener("click", (event) => {
 		let element = document.elementFromPoint(event.x, event.y);
 		element.parentNode.removeChild(element);
 	});
+}
+
+function function9() {
+	let element = null;
+	document.addEventListener("mousedown", (event) => {
+		element = document.elementFromPoint(event.x, event.y);
+	});
+	
+	document.addEventListener("mouseup", (event) => {
+		element = null;
+	})
+	
+	document.addEventListener("mousemove", (event) => {
+		if(!element) return;
+		element.style.position = "absolute";
+		element.style.display = "block";
+		element.style.left = event.x + "px";
+		element.style.top = event.y + "px";
+	})
 }
