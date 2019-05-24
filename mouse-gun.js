89eef33ac3e48d87b89165b73149b62dc7551f21
Onisko <Onisko@MacBook.local>
2019-05-24 19:59:37 +0300

    next fixed

--- mouse-gun.js
+++ mouse-gun.js
@@ -2,33 +2,56 @@
 
  //task 6
 const circle = document.createElement('div');
+let mouseButtonState = false;
 
-function circleMove(event) {
-  circle.style = `top:${event.clientY - circle.clientHeight / 2}px; left:${event.clientX - circle.clientWidth / 2}px`;
+function setCircleCoords (event) {
+  const topCoord = `${event.clientY - circle.clientHeight / 2}px`;
+  const leftCoord = `${event.clientX - circle.clientWidth / 2}px`;
+  circle.style = `top:${topCoord}; left:${leftCoord}`;
 }
 
-document.addEventListener('mousedown', (event)=>{
+function mouseDown (event) {
+  mouseButtonState = true;
   circle.classList.toggle('circle');
-  circle.style = `top:${event.clientY - circle.clientHeight / 2}px; left:${event.clientX - circle.clientWidth / 2}px`;
+  setCircleCoords(event);
   document.body.appendChild(circle);
+}
 
-  document.addEventListener('mousemove', circleMove);
-});
+function mouseMove (event) {
+  if (mouseButtonState) {
+    setCircleCoords(event);
+  }
+}
 
-document.addEventListener('mouseup', (event)=> {
+function mouseUp (event) {
+  mouseButtonState = false;
   circle.classList.toggle('circle');
+}
 
-  document.removeEventListener('mousemove', circleMove);
-})
-//task 7
-document.addEventListener('mousedown', (event)=> {
-  document.addEventListener('mousemove',function writingCoords(event) {
+document.addEventListener('mousedown', mouseDown);
+document.addEventListener('mousemove', mouseMove);
+document.addEventListener('mouseup', mouseUp);
+
+// task 7
+let mouseButtonState = false;
+
+function mouseDown (event) {
+  mouseButtonState = true;
+}
+
+function mouseMove (event) {
+  if (mouseButtonState) {
     console.log(`${event.clientX}: ${event.clientY}`);
-    document.addEventListener('mouseup', (event)=> {
-      document.removeEventListener('mousemove', writingCoords);
-    })
-  });
-});
+  }
+}
+
+function mouseUp (event) {
+  mouseButtonState = false;//
+}
+document.addEventListener('mousedown', mouseDown);
+document.addEventListener('mousemove', mouseMove);
+document.addEventListener('mouseup', mouseUp);
+
 // task 8
 document.addEventListener('click', (event) => {
     const destroyedElem = document.elementFromPoint(event.clientX, event.clientY);
