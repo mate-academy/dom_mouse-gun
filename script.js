91787306cea5c208be2d9919142c13f21da313d1
VladimirTi <tytmes@mail.ru>
2019-05-26 01:47:51 +0300

    correction

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelector('html').addEventListener('click', event => {
 //task 8
 const html = document.querySelector('html');
 
-const itemPosition = (event) => {
+const draggingElement  = (event) => {
   event.preventDefault()
   const item = document.elementFromPoint(event.clientX, event.clientY)
   item.style.position = "absolute"
@@ -21,48 +21,10 @@ const itemPosition = (event) => {
 html.addEventListener('mousedown', event => {
   event.preventDefault()
   itemPosition(event)
-  html.addEventListener('mousemove', itemPosition)
+  html.addEventListener('mousemove', draggingElement)
   document.body.appendChild(element)
 });
 
 html.addEventListener('mouseup', event => {
-  html.removeEventListener('mousemove', itemPosition)
+  html.removeEventListener('mousemove', draggingElement)
 });
-//
-document.addEventListener('mousedown', (event) => {
-  let element = document.elementFromPoint(event.clientX, event.clientY);
-  
-  let coords = getCoords(element);
-  let shiftX = event.pageX - coords.left;
-  let shiftY = event.pageY - coords.top;
-
-  element.style.position = 'absolute';
-  moveElement(event);
-  document.body.appendChild(element);
-
-  element.style.zIndex = 1000;
-
-  function moveElement(event) {
-    element.style.left = event.pageX - shiftX + 'px';
-    element.style.top = event.pageY - shiftY + 'px';
-  }
-
-  document.onmousemove = moveElement;
-
-  element.onmouseup = function() {
-    document.onmousemove = null;
-    element.onmouseup = null;
-  };
-  
-  element.ondragstart = function() {
-    return false;
-  };
-  
-  function getCoords(elem) {
-    let box = elem.getBoundingClientRect();
-    return {
-      top: box.top + pageYOffset,
-      left: box.left + pageXOffset
-    };
-  }
-})
